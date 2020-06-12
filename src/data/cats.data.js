64c61dd7fdc31088da5cec53235e6f192aa3ef47
espe-on <eljoe0@googlemail.com>
2020-06-12 16:45:57 +0100

    moved data to .js file

---
+++ src/data/cats.data.js
@@ -0,0 +1,109 @@
+﻿export const catsArray = [
+    {
+        id : "cat01",
+        name : "Lotte",
+        age : "Nearly One Years Old",
+        personality : "Puppy Cat",
+        likes : ["Being on top of the cupboard","Observing", "Playing fetch"],
+        dislikes : ["the coffee grinder"],
+        loves : ["Her Sister Sucy",""],
+        bio : "This little puppy cat always wants to be the centre of attention. she climb, she monch, she observe, she blep, she yell.",
+        socialUrl : "https://www.instagram.com/lotteandsucy/"
+    },
+    {
+        id : "cat02",
+        name : "Sucy",
+        age : "Nearly One Years Old",
+        personality : "Floppy Cat",
+        socialUrl : "https://www.instagram.com/lotteandsucy/"
+    },
+    {
+        id : "cat03",
+        name : "Martin",
+        age : "7 months old",
+        personality : "Explorer Cat",
+        likes : ["Going for walks"," Puddles", "Cuddles", "80s radio", "Round containers"],
+        dislikes : ["Closed doors"],
+        loves : ["Being picked up and slow danced with!"],
+        bio : "Martin will bound headfirst into any adventure and he loves to explore. He is learning to walk on a lead, and he's very chatty. The world is his oyster. And that unattended water glass is now his"
+    },
+    {
+        id : "cat04",
+        name : "Zeus",
+        age : "7 months old",
+        personality : "Teddy bear cat",
+        likes : ["Grooming everyone's hair", "Laundry baskets"," Climbing trees"],
+        dislikes : ["Rainstorms"],
+        loves : ["Hugs","Watching bumblebees"],
+        bio : "Zeus is very much the baby of the family, and he's cautious about new things. He is incredibly affectionate and will repeatedly headbutt you until you respond with pats. You will often find him doing his little frog-sit, and facing a completely unknown and mysterious focal point in the room."
+    },
+    {
+        id : "cat05",
+        name : "Casper",
+        age : "Two years old",
+        personality : "Anger floof",
+        likes : ["Taking your food","Shouting at 3am", "Checking what's in the bin bag", "leaving hair on everything"],
+        dislikes : ["The vacuum"]
+    },
+    {
+        id : "cat06",
+        name : "Raphael",
+        age : "Nearly seven",
+        personality : "Shouty boi",
+        likes : ["Chasing ankles", "One pile of leaves outside the front door", "Headbutting faces at 3am"],
+        dislikes : ["People", "Loud noises", "Being laughed at"]
+    },
+    {
+        id : "cat07",
+        name : "Louie",
+        age : "8 years old",
+        personality : "Street cat",
+        likes : ["The sun", "Playing with a string", "Food"],
+        dislikes : ["Dogs","Rain"],
+        loves : ["Felix pouches"],
+        bio : "This fella is very good at communicating exactly what he wants and everyone on the street knows his name when he sits on our front fence"
+    },
+    {
+        id : "cat08",
+        name : "BoBo",
+        age : "Sixteen years old",
+        personality : "Skat cat",
+        likes : ["Eating", "Rolling in dirt after grooming "],
+        dislikes : ["sister"],
+        loves : ["sister"],
+        bio : "Ladies cat, loves cuddles, too old to care about anything, will probably ask for pets from a burglar, cannot catch mice anymore but good at fur balls"
+    },
+    {
+        id : "cat09",
+        name : "Blue",
+        age : "Eight years old",
+        personality : "Guard cat",
+        likes : ["Chasing but never catching small animals", "Stealing food", "Chin rubs"],
+        dislikes : ["People sitting in her spot", "The siamese cat next door", "Most cat food (only the best for this princess"]
+    },
+    {
+        id : "cat10",
+        name : "Suki",
+        age : "Fourteen years old",
+        personality : "A bit bitey",
+        likes : ["Her favourite human (her dad)", "Sunbathing", "Biting strangers"],
+        dislikes : ["Strangers","Being woken up"]
+    },
+    {
+        id : "cat11",
+        name : "Tilly",
+        age : "Sixteen years old",
+        personality : "Cuddly gal",
+        likes : ["Climbing roofs", "Going into dark places", "Mewing at birds", "Sleeping on your head"],
+        dislikes : ["Dogs", "Being carried downstairs", "Toddlers"]
+    },
+    {
+        id : "cat12",
+        name : "Trevor",
+        age : "Maybe ten?",
+        personality : "Cute dickhead",
+        likes : ["Making biscuits on your leg", "Drinking from the tap", "Peas", "Belly scratches"],
+        dislikes : ["The derp faced cat who lives two doors away", "The vacuum", "Belly scratches"],
+        socialUrl : "https://www.instagram.com/lotteandsucy/"
+    }
+]
