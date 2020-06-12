d38102285e9b878fbf5096a88bd5c505ce3251c8
espe-on <eljoe0@googlemail.com>
2020-06-12 16:46:50 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {SearchBox} from "./components/search-box/search-box.component"
+import {catsArray} from "./data/cats.data";
+import {CardList} from "./components/card-list/card-list.component";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cats: catsArray,
+            searchField: ""
+        }
+    }
+
+    
+
+    handleChange = event => {
+        this.setState({searchField: event.target.value})
+    }
+
+    render() {
+        const {cats, searchField} = this.state;
+        const filteredCats = cats.filter(cat =>
+            cat.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+        
+        return (
+            <div className="App">
+                <h1>My Cat Index</h1>
+                <SearchBox
+                    placeholder="search cats"
+                    handleChange={this.handleChange}
+                />
+                <CardList cats={filteredCats}/>
+                    
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,13 @@
+﻿﻿import React from "react";
+
+import "./card.styles.css";
+
+export const Card = props => (
+    <div className="card-container">
+        <img alt={`a picture of ${props.cat.name}`}
+             src={`${process.env.PUBLIC_URL}/catpics/${props.cat.name.toLowerCase()}.jpeg`}
+             className="cat-pic"
+        />
+        <h2> {props.cat.name} </h2>
+    </div>
+)
